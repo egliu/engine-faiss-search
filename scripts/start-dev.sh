8206b2854c95f8b7b01429eefa50960501e31b0d
egliu <llnatalie@163.com>
2020-05-22 17:42:48 +0800

    Add kafka&zookeeper in dev script.

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,14 +2,28 @@
 
 set -euxo pipefail
 
-docker rm -f efs || true
+docker rm -f efs kafka zk || true
 
 echo "start local dev env"
+
+docker run -d --name=zk --rm \
+  -p 127.0.0.1:2181:2181 \
+  wurstmeister/zookeeper
+
+docker run -d --name=kafka --rm \
+  --publish 9092:9092 \
+  --link zk \
+  --env KAFKA_ZOOKEEPER_CONNECT=127.0.0.1:2181 \
+  --env KAFKA_ADVERTISED_HOST_NAME=127.0.0.1 \
+  --env KAFKA_ADVERTISED_PORT=9092  \
+  wurstmeister/kafka
 
 code_root=/go/src/engine-faiss-search
 
 docker run -d --name=efs \
   --network=host \
+  --add-host="zk:127.0.0.1" \
+  --add-host="kafka:127.0.0.1" \
   -e TZ=Asia/Shanghai \
   -v ${PWD}:${code_root} \
   -w ${code_root} \
